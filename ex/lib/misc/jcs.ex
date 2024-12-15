629ab9a535a1fc537e37195ca0c561ef9c968eee
vans163 <me@vans163.com>
2024-12-15 09:28:40 -0500

    add JCS, canonical serialization

---
+++ ex/lib/misc/jcs.ex
@@ -0,0 +1,33 @@
+defmodule JCS do
+    def serialize(map) when is_map(map) do
+        map
+        |> serialize_1()
+        |> JSX.encode!()
+    end
+    defp serialize_1(map) do
+        serialize_list = fn(list, self)->
+            Enum.map(list, fn(v)->
+                cond do
+                    is_map(v)-> serialize_1(v)
+                    is_list(v)-> self.(v)
+                    true -> v
+                end
+            end)
+        end
+        Enum.map(map, fn{k,v}->
+            cond do
+                is_map(v) -> {k, serialize_1(v)}
+                is_list(v) -> {k, serialize_list.(v, serialize_list)}
+                true -> {k,v}
+            end
+        end)
+        |> Enum.sort_by(& &1)
+    end
+
+    def validate(binary) do
+        binary
+        |> JSX.decode!()
+        |> serialize()
+        |> Kernel.==(binary)
+    end
+end
