2dc0ea60168861670e1197c734f01c240ac84e8f
vans163 <me@vans163.com>
2024-12-31 20:19:40 -0500

    add computer

--- ex/lib/ex.ex
+++ ex/lib/ex.ex
@@ -4,32 +4,48 @@ defmodule Ama do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    IO.inspect Application.app_dir(:ama, "priv/index.html") 
+    #IO.inspect Application.app_dir(:ama, "priv/index.html") 
 
     supervisor = Supervisor.start_link([
       {DynamicSupervisor, strategy: :one_for_one, name: Ama.Supervisor, max_seconds: 1, max_restarts: 999_999_999_999}
     ], strategy: :one_for_one)
 
     path = Path.join([Application.fetch_env!(:ama, :work_folder), "mnesia_kv/"])
-    MnesiaKV.load(%{
-        TX => %{index: [:uuid, :status, :nonce, :type, :prev_tx]},
-      },
-      %{path: path}
-    )
+    #MnesiaKV.load(%{
+    #    TX => %{index: [:uuid, :status, :nonce, :type, :prev_tx]},
+    #  },
+    #  %{path: path}
+    #)
+
+    <<six::6-binary, _::binary>> = Application.fetch_env!(:ama, :challenge_signature)
+    IO.inspect {:challenge_solved, six}
+
+    IO.puts "Loading chain.."
+    BIC.Base.init()
+    Blockchain.load_chain_on_init()
+    
+    IO.puts "Initing TXPool.."
+    TXPool.init()
 
     {:ok, _} = DynamicSupervisor.start_child(Ama.Supervisor, %{id: PG, start: {:pg, :start_link, []}})
     {:ok, _} = DynamicSupervisor.start_child(Ama.Supervisor, %{id: PGWSPanel, start: {:pg, :start_link, [PGWSPanel]}})
     
-    #web panel
-    ip4 = Application.fetch_env!(:ama, :http_ip4)
-    port = Application.fetch_env!(:ama, :http_port)
-    {a,b,c,d} = ip4
-    ip4_string = "#{a}.#{b}.#{c}.#{d}"
-    IO.puts "started http-api on #{ip4_string}:#{port}"
+    {:ok, _} = DynamicSupervisor.start_child(Ama.Supervisor, %{id: Computor, start: {Computor, :start_link, []}})
+    
+    ip4 = Application.fetch_env!(:ama, :udp_ipv4_tuple)
+    port = Application.fetch_env!(:ama, :udp_port)
+    {:ok, _} = DynamicSupervisor.start_child(Ama.Supervisor, %{id: NodeGen, start: {NodeGen, :start_link, [ip4, port]}})
 
-    {:ok, _} = DynamicSupervisor.start_child(Ama.Supervisor, %{
-      id: Photon.GenTCPAcceptor, start: {Photon.GenTCPAcceptor, :start_link, [ip4, port, Ama.MultiServer]}
-    })
+    #web panel
+    #ip4 = Application.fetch_env!(:ama, :http_ip4)
+    #port = Application.fetch_env!(:ama, :http_port)
+    #{a,b,c,d} = ip4
+    #ip4_string = "#{a}.#{b}.#{c}.#{d}"
+    #IO.puts "started http-api on #{ip4_string}:#{port}"
+
+    #{:ok, _} = DynamicSupervisor.start_child(Ama.Supervisor, %{
+    #  id: Photon.GenTCPAcceptor, start: {Photon.GenTCPAcceptor, :start_link, [ip4, port, Ama.MultiServer]}
+    #})
 
     supervisor
   end
---
+++ ex/lib/node/computor.ex
@@ -0,0 +1,65 @@
+defmodule Computor do
+  use GenServer
+
+  def start() do
+    send(__MODULE__, :start)
+  end
+
+  def stop() do
+    send(__MODULE__, :stop)
+  end
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, %{enabled: false}, name: __MODULE__)
+  end
+
+  def init(state) do
+    :erlang.send_after(1000, self(), :tick)
+    {:ok, state}
+  end
+
+  def handle_info(:tick, state) do
+    state = if state[:enabled] do tick(state) else state end
+    :erlang.send_after(1000, self(), :tick)
+    {:noreply, state}
+  end
+
+  def handle_info(:start, state) do
+    state = Map.put(state, :enabled, true)
+    {:noreply, state}
+  end
+
+  def handle_info(:stop, state) do
+    state = Map.put(state, :enabled, false)
+    {:noreply, state}
+  end
+
+  def tick(state) do
+    IO.puts "computor running #{DateTime.utc_now()}"
+    pk_raw = Application.fetch_env!(:ama, :trainer_pk)
+    pk_b58 = Application.fetch_env!(:ama, :trainer_pk_b58)
+
+    coins = BIC.Coin.balance(Base58.encode(pk_raw))
+    hasExecCoins = coins >= BIC.Coin.to_flat(1)
+    sol = if !hasExecCoins do
+        UPOW.compute_for(Base58.decode("4MXG4qor6TRTX9Wuu9TEku7ivBtDooNL55vtu3HcvoQH"), pk_raw)
+    else
+        UPOW.compute_for(pk_raw, pk_raw)
+    end
+    cond do
+        !sol -> nil
+        !hasExecCoins ->
+            IO.puts "🔢 tensor matmul complete! broadcasting sol.."
+            NodeGen.send_sol(sol)
+        true ->
+            sk_raw = Application.fetch_env!(:ama, :trainer_sk)
+            packed_tx = TX.build_transaction(sk_raw, Blockchain.height(), "Trainer", "submit_sol", [Base58.encode(sol)])
+            %{hash: hash} = TX.unwrap(packed_tx)
+            IO.puts "🔢 tensor matmul complete! tx #{hash}"
+
+            TXPool.insert(packed_tx)
+            NodeGen.send_txpool(packed_tx)
+    end
+    state
+  end 
+end
