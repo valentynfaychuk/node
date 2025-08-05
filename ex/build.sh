cec939a3d589cbe04cb1dd712706ca96e3ba82b3
vans163 <me@vans163.com>
2025-08-05 12:15:38 -0400

    small fixes

--- ex/build.sh
+++ ex/build.sh
@@ -4,7 +4,7 @@ podman run -it --rm -v .:/root/node --entrypoint bash erlang_builder -c "echo 'b
     && cd /root/node \
     && export MIX_ENV=prod \
     && export CC=clang-19 \
-    && export RUSTFLAGS='-C target-feature=+avx2,+sse4.2 -C opt-level=3' \
+    && export RUSTFLAGS='-C target-feature=+avx2,+sse4.2,+fma -C opt-level=3' \
     && mix deps.get \
     && mix release \
     && cp _build/prod/rel/bakeware/ama amadeusd"
--- ex/lib/misc/vanity_generator.ex
+++ ex/lib/misc/vanity_generator.ex
@@ -1,4 +1,5 @@
 defmodule VanityGenerator do
+  # VanityGenerator.parallel("77777", false, true, 1_000_000)
   def parallel(prefix, skip_leading \\ true, case_insensitive \\ true, max_tries \\ 10000) do
     stream = Task.async_stream(1..max_tries, fn(_)-> go(prefix, skip_leading, case_insensitive, max_tries) end, [{:ordered, false}, {:timeout, :infinity}])
     Enum.find(stream, & &1)
@@ -10,6 +11,10 @@ defmodule VanityGenerator do
     full_key = BlsEx.get_public_key!(sk) |> Base58.encode()
     key = if skip_leading do <<_, rest::binary>> = full_key; rest else full_key end
     <<key_slice::binary-size(byte_size(prefix)), _::binary>> = key
+    <<key_slice_close::binary-size(byte_size(prefix)-1), _::binary>> = key
+    if String.starts_with?(prefix, key_slice_close) do
+      IO.inspect {full_key, Base58.encode(sk)}
+    end
     cond do
       iteration > max_tries -> nil
       case_insensitive and String.upcase(key_slice) == String.upcase(prefix) -> {full_key, Base58.encode(sk)}
