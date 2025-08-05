299a8017b04c814b24f751a0b830bf2ee23e9bb4
vans163 <me@vans163.com>
2025-08-05 11:05:29 -0400

    parallel vanity

--- ex/lib/misc/vanity_generator.ex
+++ ex/lib/misc/vanity_generator.ex
@@ -1,13 +1,15 @@
 defmodule VanityGenerator do
+  def parallel(prefix, skip_leading \\ true, case_insensitive \\ true, max_tries \\ 10000) do
+    stream = Task.async_stream(1..max_tries, fn(_)-> go(prefix, skip_leading, case_insensitive, max_tries) end, [{:ordered, false}, {:timeout, :infinity}])
+    Enum.find(stream, & &1)
+  end
+
   def go(prefix, skip_leading \\ true, case_insensitive \\ true, max_tries \\ 10000, iteration \\ 0) do
     iteration = iteration + 1
     sk = :crypto.strong_rand_bytes(64)
     full_key = BlsEx.get_public_key!(sk) |> Base58.encode()
     key = if skip_leading do <<_, rest::binary>> = full_key; rest else full_key end
     <<key_slice::binary-size(byte_size(prefix)), _::binary>> = key
-    if rem(iteration, 1000) == 0 do
-      IO.inspect {prefix, key_slice, key}
-    end
     cond do
       iteration > max_tries -> nil
       case_insensitive and String.upcase(key_slice) == String.upcase(prefix) -> {full_key, Base58.encode(sk)}
