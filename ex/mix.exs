a1593a575e275a0a548eed98353262a223c8764e
vans163 <me@vans163.com>
2025-12-14 17:05:36 -0500

    v1.3.9

--- ex/mix.exs
+++ ex/mix.exs
@@ -6,7 +6,7 @@ defmodule Ama.MixProject do
   def project do
     [
       app: @app,
-      version: "1.3.8",
+      version: "1.3.9",
       elixir: ">= 1.19.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
