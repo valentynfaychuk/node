a56f128d126ce01640823476d8bc1e2a88ab7135
Valentyn Faychuk <valy@faychuk.com>
2025-12-30 05:36:28 +0200

    cleanup examples
    Signed-off-by: Valentyn Faychuk <valy@faychuk.com>

--- contract_samples/rust/build_and_validate.sh
+++ contract_samples/rust/build_and_validate.sh
@@ -2,19 +2,16 @@
 set -e
 
 cargo build -p amadeus-sdk --example counter --target wasm32-unknown-unknown --release
-cargo build -p amadeus-sdk --example counter_macro --target wasm32-unknown-unknown --release
 cargo build -p amadeus-sdk --example deposit --target wasm32-unknown-unknown --release
 cargo build -p amadeus-sdk --example coin --target wasm32-unknown-unknown --release
 cargo build -p amadeus-sdk --example nft --target wasm32-unknown-unknown --release
 
 wasm-opt -Oz --enable-bulk-memory target/wasm32-unknown-unknown/release/examples/counter.wasm -o target/wasm32-unknown-unknown/release/examples/counter_opt.wasm
-wasm-opt -Oz --enable-bulk-memory target/wasm32-unknown-unknown/release/examples/counter_macro.wasm -o target/wasm32-unknown-unknown/release/examples/counter_macro_opt.wasm
 wasm-opt -Oz --enable-bulk-memory target/wasm32-unknown-unknown/release/examples/deposit.wasm -o target/wasm32-unknown-unknown/release/examples/deposit_opt.wasm
 wasm-opt -Oz --enable-bulk-memory target/wasm32-unknown-unknown/release/examples/coin.wasm -o target/wasm32-unknown-unknown/release/examples/coin_opt.wasm
 wasm-opt -Oz --enable-bulk-memory target/wasm32-unknown-unknown/release/examples/nft.wasm -o target/wasm32-unknown-unknown/release/examples/nft_opt.wasm
 
 curl -X POST -H "Content-Type: application/octet-stream" --data-binary @target/wasm32-unknown-unknown/release/examples/counter_opt.wasm https://mainnet-rpc.ama.one/api/contract/validate
-curl -X POST -H "Content-Type: application/octet-stream" --data-binary @target/wasm32-unknown-unknown/release/examples/counter_macro_opt.wasm https://mainnet-rpc.ama.one/api/contract/validate
 curl -X POST -H "Content-Type: application/octet-stream" --data-binary @target/wasm32-unknown-unknown/release/examples/deposit_opt.wasm https://mainnet-rpc.ama.one/api/contract/validate
 curl -X POST -H "Content-Type: application/octet-stream" --data-binary @target/wasm32-unknown-unknown/release/examples/coin_opt.wasm https://mainnet-rpc.ama.one/api/contract/validate
 curl -X POST -H "Content-Type: application/octet-stream" --data-binary @target/wasm32-unknown-unknown/release/examples/nft_opt.wasm https://mainnet-rpc.ama.one/api/contract/validate
